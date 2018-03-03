0cfd928c74e5ef2b6ab3d1232fdb81af86c8396d
lanzero <thuanzero94@gmail.com>
2018-03-03 09:53:46 +0700

    DONE Chapter 8!

--- app/src/main/java/com/foxconn/alan/alann_criminalintent/CrimeFragment.java
+++ app/src/main/java/com/foxconn/alan/alann_criminalintent/CrimeFragment.java
@@ -8,7 +8,12 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+
+import java.text.DateFormat;
 
 /**
  * Created by alan on 01/03/2018.
@@ -19,6 +24,8 @@ public class CrimeFragment extends Fragment {
 
     private Crime mCrime;
     private EditText mEditText_TitleField;
+    private Button mBtnDate;
+    private CheckBox mCheckBoxSolved;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -46,6 +53,19 @@ public class CrimeFragment extends Fragment {
             @Override
             public void afterTextChanged(Editable s) {
 
+            }
+        });
+
+        mBtnDate = (Button)v.findViewById(R.id.crime_date);
+        mBtnDate.setText(DateFormat.getDateInstance(DateFormat.FULL).format(mCrime.getDate()));
+        mBtnDate.setEnabled(false);
+
+        mCheckBoxSolved = (CheckBox)v.findViewById(R.id.crime_solved);
+        mCheckBoxSolved.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // Set the crime's solved property
+                mCrime.setSolved(isChecked);
             }
         });
 
